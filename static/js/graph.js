ff43b8ecb6d8963983849a2c0fb2c82b3290a75d
Mateusz Delegacz <deli@Mateuszs-MacBook-Pro.local>
2019-05-06 20:46:12 +0100

    Added series linie chart, Updated Layout and Styling, Resolved CORS API Issue

--- static/js/graph.js
+++ static/js/graph.js
@@ -1,26 +1,35 @@
 queue()
-   //Loading CSV Data
-  .defer(d3.csv, "./data/dc.csv")
-   .await(makeChart); //Calling the function makeChart
+//Loading CSV Data
+    .defer(d3.csv, "./data/data.csv")
+    .await(makeChart); //Calling the function makeChart
 function makeChart(error, dcData) { //first param is error and not data
 
     var ndx = crossfilter(dcData); // global crossfilter
 
+
+
     //Creating a date format of just a Year
-    var FirstApperanceDate = d3.time.format("%Y").parse;
+    var FirstApperanceDate = d3
+        .time
+        .format("%Y")
+        .parse;
     //Changin values to dates - Years
-    dcData.forEach(function(d) {
+    dcData.forEach(function (d) {
         d.FirstApperance = FirstApperanceDate(d.FirstApperance);
     });
-    //Caling graphs function to be rendered
+    //Caling graphs function to be rendered Selectors
     show_universum_selector(ndx);
     show_category_selector(ndx);
     show_alignment(ndx)
+    //Graphs
     dc_heros_by_gender(ndx);
+    heros_by_alignment(ndx);
     power_distribution_by_universum(ndx);
     power_distribution_by_gender(ndx);
     heros_by_race(ndx);
     new_apperances_through_years(ndx);
+
+    apperances_t_years_by_universum(ndx, dcData);
     //Rendering Graphs
     dc.renderAll();
 }
@@ -32,13 +41,13 @@ function show_universum_selector(ndx) {
         .selectMenu("#univers-selector")
         .dimension(dim)
         .group(group);
-        
+
 }
 function show_category_selector(ndx) {
     var dim = ndx.dimension(dc.pluck('Category'));
     var group = dim.group();
 
-   dc
+    dc
         .selectMenu("#category-selector")
         .dimension(dim)
         .group(group);
@@ -47,7 +56,7 @@ function show_alignment(ndx) {
     var dim = ndx.dimension(dc.pluck('Alignment'));
     var group = dim.group();
 
-     dc
+    dc
         .selectMenu("#alignment-selector")
         .dimension(dim)
         .group(group);
@@ -56,7 +65,7 @@ function dc_heros_by_gender(ndx) {
     var dim = ndx.dimension(dc.pluck('Gender'));
     var group = dim.group();
 
-   one = dc
+    one = dc
         .pieChart("#gender-balance-dc")
         .width(500)
         .dimension(dim)
@@ -64,8 +73,9 @@ function dc_heros_by_gender(ndx) {
         .innerRadius(50)
         .legend(dc.legend().x(380).y(70).itemHeight(13).gap(5))
         .renderLabel(false)
-        .ordinalColors(["#56B2EA","#E064CD","#F8B700","#78CC00","#7B71C5"])
-        
+        .ordinalColors(["#7CCFCD", "#E58579", "#822F56"])
+        .transitionDuration(1000)
+
 }
 function heros_by_race(ndx) {
     var dim = ndx.dimension(dc.pluck('Category'));
@@ -77,40 +87,74 @@ function heros_by_race(ndx) {
         .dimension(dim)
         .group(group)
         .innerRadius(50)
-        .legend(dc.legend().x(380).y(70).itemHeight(13).gap(5))
+        .legend(dc.legend().x(380).y(40).itemHeight(13).gap(5))
         .renderLabel(false)
+        .ordinalColors([
+            "#2F2936",
+            "#822F56",
+            "#C54961",
+            "#E58579",
+            "#F3AB91",
+            "#7CCFCD",
+            "#B5EBE8"
+        ])
+        .transitionDuration(1000)
+}
+function heros_by_alignment(ndx) {
+    var dim = ndx.dimension(dc.pluck('Alignment'));
+    var group = dim.group();
+
+    dc
+        .pieChart("#alignment-procentage")
+        .width(500)
+        .dimension(dim)
+        .group(group)
+        .innerRadius(50)
+        .legend(dc.legend().x(380).y(60).itemHeight(13).gap(5))
+        .renderLabel(false)
+        .ordinalColors([
+            "#2F2936",
+            "#822F56",
+            "#C54961",
+            "#E58579",
+            "#F3AB91",
+            "#7CCFCD",
+            "#B5EBE8"
+        ])
+        .transitionDuration(1000)
 }
 function new_apperances_through_years(ndx, dcData) {
+    var dim = ndx.dimension(dc.pluck("FirstApperance"));
 
+    var numberofApperances = dim
+        .group()
+        .reduceCount();
+    var minDate = dim
+        .bottom(1)[0]
+        .FirstApperance;
+    var maxDate = dim
+        .top(1)[0]
+        .FirstApperance;
+    
+    //var dcApperances = numberofApperances.reduceCount('DC Comics');
 
-   var dim = ndx.dimension(dc.pluck("FirstApperance"));
+    dc
+        .lineChart("#apperance_t_years")
+        .width(800)
+        .height(300)
+        .elasticY(true)
+        .x(d3.time.scale().domain([minDate, maxDate]))
+        .elasticX(true)
+        .brushOn(false)
+        .yAxisLabel("Number of Apperances")
+        .dimension(dim)
+        .group(numberofApperances)
+        .renderArea(true)
+        .ordinalColors(["#C54961"])
+        .renderDataPoints(numberofApperances)
 
-   var numberofApperances = dim
-       .group()
-       .reduceCount();
-   var minDate = dim.bottom(1)[0].FirstApperance;
-   var maxDate = dim.top(1)[0].FirstApperance;
-
-   dc
-       .lineChart("#apperance_t_years")
-       //.attr("class", "line")
-       //.attr("fill", "none")
-       //.attr("stroke", "#000")
-       .width(800)
-       .height(300)
-       .elasticY(true)
-       //.x(d3.scale.linear().domain([1930, 2018]))
-       .x(d3.time.scale().domain([minDate, maxDate]))
-       .brushOn(false)
-       
-       .yAxisLabel("Number of Apperances")
-       .xAxisLabel("Year")
-       .dimension(dim)
-       
-       .group(numberofApperances)
-       .renderArea(true)
-       .ordinalColors(["#56B2EA","#E064CD","#F8B700","#78CC00","#7B71C5"])
 }
+
 function power_distribution_by_universum(ndx) {
     var universumColors = d3
         .scale
@@ -152,7 +196,7 @@ function power_distribution_by_gender(ndx) {
         .scale
         .ordinal()
         .domain(["female", "male", ["unidentified"]])
-        .range(["pink", "blue", "red"]);
+        .range(["#E064CD", "#56B2EA", "#F8B700"]);
 
     var pDim = ndx.dimension(dc.pluck("Power"));
     var uDim = ndx.dimension(function (d) {
@@ -182,5 +226,46 @@ function power_distribution_by_gender(ndx) {
         .dimension(pDim)
         .group(universeGroup)
         .margins({top: 10, right: 50, bottom: 75, left: 75});
+}
+function apperances_t_years_by_universum(ndx, dcData) {
+    dcData.forEach(function(x) {
+        if (x.Publisher == 'Marvel Comics') {
+            x.newdata = 1;
+        }   else {
+            x.newdata =2;
+        }
+    });
+
+    var dim = ndx.dimension(function(data){
+        return [data.Publisher, data.FirstApperance];
+    });
+    var group = dim.group().reduceCount();
+
+    var minDate = dim.bottom(1)[0].FirstApperance;
+    var maxDate = dim.top(1)[0].FirstApperance;
+
+    dc.seriesChart('#apperances_t_years_by_universum')
+    .width(800)
+    .height(600)
+   // .chart(function(c) { 
+    //    return dc.lineChart(c).interpolate('cardinal').evadeDomainFilter(true); 
+  //  })
+    .x(d3.time.scale().domain([minDate, maxDate]))
+    .y(d3.scale.linear().domain([0, 30]))
+    .chart(function(c) { 
+        return dc.lineChart(c)
+                 .evadeDomainFilter(false)
+                 .xyTipsOn(true);     
+                })
+    //.elasticY(true)
+    .brushOn(false)
+    //.xAxisLabel("Height")
+    .yAxisLabel("Count")
+    .dimension(dim)
+    .group(group)
+    .seriesAccessor(function(d) { return d.key[0];})
+    .keyAccessor(function(d) { return +d.key[1]; })
+    .valueAccessor(function(d) { return +d.value; })
+    .legend(dc.legend().x(70).y(50).itemHeight(20).gap(10).horizontal(4).legendWidth(200).itemWidth(120));
 }
 
--- static/js/superHeroAPI.js
+++ static/js/superHeroAPI.js
@@ -3,30 +3,24 @@ function charInfo(character) {
     var firstname = character.biography["first-name"];
     console.log('rak');
     return `
-            <div class="ApiImage col-sm-12 col-md-4 d-inline-block">
+            <div class="ApiImage col-3">
                 <img src="${character.image.url}" width="250" height="333"/>
             </div>
-            <div class="ApiBioEntry col-sm-12 col-md-6 d-inline-block">
+            <div class="ApiBioEntry col-6">
                 <h1>${character.name}</h1>
-                <h3>${character.biography['full-name']}</h3>
-                <h4>${
-                   (() =>  { console.log(character.biography.alignment)
-                   if(character.biography.alignment = 'bad') {
-                        `<h4 class="bad">BAD</h4>`
-                    } else {`<h4 class="bad">error</h4>`}
-                })()
-
-                }</h4>
+                <span>Full Name: <h3>${character.biography['full-name']}</h3></span>
+                <span>Publisher: <h4>${character.biography.publisher}</h4></span>
+                <span>Data:</span>
                 <ul>
                     <li>Race: ${character.appearance.race}</li>
                     <li>Gender: ${character.appearance.gender}</li>
+                    <li>Origin: ${character.work.base}</li>
                     <li>Height: ${character.appearance.height[1]}</li>
                     <li>Eye Color: ${character.appearance['eye-color']}</li>
                 </ul>
             </div>
             `;
 }
-
 function getCharacterInfo(event) {
 
     function randomCharacterID() {
@@ -38,7 +32,7 @@ function getCharacterInfo(event) {
     }
     console.log(randomCharacterID());
     $
-        .when($.getJSON(`https://superheroapi.com/api/2459027580820827/${randomCharacterID()}`))
+        .when($.getJSON(`https://cors.io/?https://superheroapi.com/api/2459027580820827/${randomCharacterID()}`))
         .then(function (response) {
             console.log(response);
             $('#character-image').html(charInfo(response));
